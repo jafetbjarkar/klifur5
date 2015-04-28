27385626020a229713c28dc53eea316704216e61
Jafet Bjarkar <jafetbjarkar@gmail.com>
2015-04-28 20:43:42 +0000

    Add partner-logo to footer

---
+++ custom/partner-logo.php
@@ -0,0 +1,6 @@
+<div class="partner-logo">
+  <p>Í samstarfi við</p>
+  <a href="http://isalp.is">
+    <img src="<?php bloginfo('template_url'); ?>/images/partner-logo.svg" alt="Site partner">
+  </a>
+</div>
