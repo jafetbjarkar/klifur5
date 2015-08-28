0e65df1eac388d418d3c5359af9c57e16ce52df2
Jafet Bjarkar <jafetbjarkar@gmail.com>
2015-08-28 18:34:18 +0000

    Add bbPress to search results

--- functions_klifur.php
+++ functions_klifur.php
@@ -327,3 +327,23 @@ function my_login_logo() {
 <?php }
 add_action( 'login_enqueue_scripts', 'my_login_logo' );
 
+
+// - BBPRESS
+// Include bbPress 'topic' custom post type in WordPress' search results
+function ntwb_bbp_topic_cpt_search( $topic_search ) {
+	$topic_search['exclude_from_search'] = false;
+	return $topic_search;
+}
+add_filter( 'bbp_register_topic_post_type', 'ntwb_bbp_topic_cpt_search' );
+
+/**
+ * Include bbPress 'reply' custom post type in WordPress' search results
+ */
+function ntwb_bbp_reply_cpt_search( $reply_search ) {
+	$reply_search['exclude_from_search'] = false;
+	return $reply_search;
+}
+add_filter( 'bbp_register_reply_post_type', 'ntwb_bbp_reply_cpt_search' );
+
+
+
