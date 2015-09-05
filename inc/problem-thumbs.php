0f5453dc3dc008ebaa66fce7fbe9f91776bba056
Jafet Bjarkar <jafetbjarkar@gmail.com>
2015-09-05 21:27:59 +0000

    Problem: Change wording to Related routes

--- inc/problem-thumbs.php
+++ inc/problem-thumbs.php
@@ -1,6 +1,6 @@
 <?php /**
 *
-* Some variables available from content-problem.php: 
+* Some variables available from content-problem.php:
 * $crag_id, $sector, $stone, $type
 *
 * This file needs $args  used in WP_Query()
@@ -15,16 +15,16 @@ $wp_query = null;
 $wp_query = new WP_Query($args); ?>
 
 <div class="problem-thumbs-area" data-user-id="<?php echo get_current_user_id(); ?>">
-    <h2 class="results-heading"><?php echo $wp_query->found_posts; ?> Problems</h2>
+    <h2 class="results-heading"><?php echo $wp_query->found_posts; ?> related routes</h2>
 
-<?php if( $wp_query->have_posts() ) : ?> 
-	
+<?php if( $wp_query->have_posts() ) : ?>
+
 		<?php while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
 			<div class="problem-thumb" data-post-id="<?php the_ID(); ?>">
 				<header class="entry-header">
 					<h3 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?> <?php the_field('grade'); ?></a></h3>
 				</header><!-- .entry-header -->
-				
+
 				<?php // IMAGE ?>
 				<div class="th-image-wrapper">
 					<!-- User lists -->
@@ -45,12 +45,12 @@ $wp_query = new WP_Query($args); ?>
 				</div>
 
 				<?php the_content( ); ?>
-				
-				
+
+
 
 				<div class="gradient"></div> <!-- just to add gradient to bottom -->
             </div> <!-- .problem-thumb -->
 		<?php endwhile; ?>
-		
+
 	</div> <!-- .problem-thumb-area -->
 <?php endif; ?>
