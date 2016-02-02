a217369fc5bf2f76645c9b8bfc09789e7cf0b3ba
Jafet Bjarkar <jafetbjarkar@gmail.com>
2016-02-02 20:26:47 +0000

    Recover sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The sidebar containing the secondary widget area
+ *
+ * Displays on posts and pages.
+ * If no active widgets are in this sidebar, hide it completely.
+ *
+ * If this file is not present the Main widget (footer) area fuckes up.
+ */
+
+if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+  <!-- [PATH] sidebar.php -->
+	<div id="tertiary" class="sidebar-container" role="complementary">
+		<div class="sidebar-inner">
+			<div class="widget-area">
+				<?php dynamic_sidebar( 'sidebar-2' ); ?>
+			</div><!-- .widget-area -->
+		</div><!-- .sidebar-inner -->
+	</div><!-- #tertiary -->
+<?php endif; ?>
