c560af6e0eacda58215c9120985cc0d829192627
Jafet <jafetbjarkar@gmail.com>
2018-03-17 16:57:27 +0000

    fix: missing checkmarks

--- config/data.php
+++ config/data.php
@@ -13,7 +13,7 @@
     // Crag google map
     $map_embed = '<iframe width="640" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps/ms?msid=205265335858135699416.00048240a25d103bca6b8&amp;msa=0&amp;ie=UTF8&amp;t=m&amp;ll=65.007224,-19.248047&amp;spn=3.715753,15.358887&amp;z=6&amp;output=embed"></iframe>';
     // ACF climbing type categories
-    $climbing_types = ['Boulder', 'Sport Climbing', 'Traditional climbing'];
+    $climbing_types = ['Boulder', 'Sport climbing', 'Traditional climbing'];
     // The add route link markup
     $add_route_link = '<p><a href="http://klifur.is/wp-admin/post-new.php">Veist þú um eina?"</a></p>';
 
